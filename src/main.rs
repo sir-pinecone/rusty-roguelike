327fa687f8a557935267fadc9c31c982397dd382
Michael Campagnaro <mikecampo@gmail.com>
2017-03-06 14:53:25 -0500

    Refactor with a generic objects struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,48 @@
 extern crate tcod;
 
 use tcod::console::*;
-use tcod::colors;
+use tcod::colors::{self, Color};
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 30;
 
-fn handle_input(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object {
+            x: x,
+            y: y,
+            char: char,
+            color: color,
+        }
+    }
+
+    /* Move by a given amount */
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    /* Draw the character that represents this object at its current position */
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+
+    /* Erase the character that represents this object */
+    pub fn clear(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
+
+fn handle_input(root: &mut Root, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -23,10 +58,10 @@ fn handle_input(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool
         Key { code: Escape, .. } => return true,
 
         // Movement
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
 
         _ => {}
     }
@@ -45,12 +80,15 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let wizard = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
+
+    let mut objects = [player, wizard];
 
     while !root.window_closed() {
-        con.set_default_foreground(colors::WHITE);
-        con.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        for object in &objects {
+            object.draw(&mut con);
+        }
 
         blit(&mut con,
              (0, 0),
@@ -60,12 +98,15 @@ fn main() {
              1.0,
              1.0);
 
-        // Clear the previous player position
-        con.put_char(player_x, player_y, ' ', BackgroundFlag::None);
-
         root.flush();
 
-        let exit = handle_input(&mut root, &mut player_x, &mut player_y);
+        // Erase objects at their old locations before moving
+        for object in &objects {
+            object.clear(&mut con);
+        }
+
+        let player = &mut objects[0];
+        let exit = handle_input(&mut root, player);
         if exit {
             break;
         }
