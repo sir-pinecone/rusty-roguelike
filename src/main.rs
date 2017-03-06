50e7c9aeef92522634cc98b251be2a0fb19118e3
Michael Campagnaro <mikecampo@gmail.com>
2017-03-06 14:34:25 -0500

    Render to an offscreen console

--- src/main.rs
+++ src/main.rs
@@ -41,19 +41,29 @@ fn main() {
         .title("Rusty Roguelike")
         .init();
 
+    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+
     tcod::system::set_fps(LIMIT_FPS);
 
     let mut player_x = SCREEN_WIDTH / 2;
     let mut player_y = SCREEN_HEIGHT / 2;
 
     while !root.window_closed() {
-        root.set_default_foreground(colors::WHITE);
-        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        con.set_default_foreground(colors::WHITE);
+        con.put_char(player_x, player_y, '@', BackgroundFlag::None);
 
-        root.flush();
+        blit(&mut con,
+             (0, 0),
+             (SCREEN_WIDTH, SCREEN_HEIGHT),
+             &mut root,
+             (0, 0),
+             1.0,
+             1.0);
 
         // Clear the previous player position
-        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+        con.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+
+        root.flush();
 
         let exit = handle_input(&mut root, &mut player_x, &mut player_y);
         if exit {
