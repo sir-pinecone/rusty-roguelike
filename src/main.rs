0cfc4f9de2eed4141e0baccc209a80bc8503a876
Michael Campagnaro <mikecampo@gmail.com>
2017-04-10 17:57:49 -0400

    Tweak some tile colors

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,9 @@ const FOV_LIGHT_WALLS: bool = true;
 const TORCH_RADIUS: i32 = 10;
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
-const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b: 50 };
+const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 100, b: 90 };
 const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
-const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50 };
+const COLOR_LIGHT_GROUND: Color = Color { r: 180, g: 160, b: 108 };
 
 
 struct ThreadContext {
